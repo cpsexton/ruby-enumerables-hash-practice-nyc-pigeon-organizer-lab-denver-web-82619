4ec00913f1d4818389b565f6061c381c1f3db6f9
Christopher Sexton <cpsexton@gmail.com>
2019-08-20 14:42:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, h), result|
-    h.each do |value, names|
+  data.each_with_object({}) do |(key, i), result|
+    i.each do |value, names|
       names.each do |name|
         result[name] ||= {}
         result[name][key] ||= []
